4f5cf93aedbecf8c37e753b282cfca867a4975f5
Usman Aqil <aqilusman45@gmail.com>
2024-04-05 14:01:56 +0500

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,19 @@
 import clsx from 'clsx'
 
 export const Button = ({
-  otherClasses, onClick
+  otherClasses, onClick, label, varient = 'primary'
 }) => {
 
   const buttonClasses = clsx(
-    otherClasses
+    otherClasses,
+    varient === 'primary' && 'text-white bg-blue-600 rounded-sm px-6 py-2',
+    varient === 'secondary' && 'text-gray-900 px-6 py-2 border border-black rounded-sm bg-transparent',
+
   )
-  
+  console.log(varient, 'varient');
   return (
     <button onClick={onClick} className={buttonClasses} data-testid='button'>
-      new
+      <span>{label}</span>
     </button>
   )
 }
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,13 +1,35 @@
 import * as React from 'react'
+import mocks from './mocks'
 
 import Button from './Button'
 
 export default {
   title: 'Components/Button',
   component: Button,
+  varient: {
+    defaultValue: 'primary',
+    Options: [
+      'primary',
+      'secondary'
+    ]
+  }
 }
 
 const Template = (args) => <Button {...args} />
 
+export const ButtonPrimary = Template.bind({})
+ButtonPrimary.args = {
+  variant: 'primary',
+  label: 'Get A Quote',
+  // onClick
+}
+
+export const SecondaryButton = Template.bind({})
+SecondaryButton.args = {
+  variant: 'secondary',
+  label: 'Get A Quote',
+  // onClick
+}
+
 export const Default = Template.bind({})
-Default.args = {}
+Default.args = {...mocks}
